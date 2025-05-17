7906d5ebbe2fcdb8fced5315e715c390658953c3
nippon935 <nippongrill935@gmail.com>
2025-05-16 22:46:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  document.getElementById("about").addEventListener("click", () => {
+    document.getElementById("about-sec").scrollIntoView({ behavior: "smooth" });
+  });
+
+
+  document.getElementById("specials").addEventListener("click", () => {
+    document.getElementById("special-sec").scrollIntoView({ behavior: "smooth" });
+  });
+
+  document.getElementById("gallery").addEventListener("click", () => {
+    document.getElementById("gallery-sec").scrollIntoView({ behavior: "smooth" });
+  });
+
+  document.getElementById("contact-location").addEventListener("click", () => {
+    document.getElementById("contact-sec").scrollIntoView({ behavior: "smooth" });
+  });
+  
+
+
+  const gallery = document.getElementById('gallery');
+  setInterval(() => {
+    const firstImages = gallery.querySelector('.gallery-img');
+    if (firstImages) {
+      gallery.appendChild(firstImages);
+    }
+  }, 100);
+
+});
+
+
+
+  
